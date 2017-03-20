52ad4b9f0274c156e4152774d9ae7d1198965666
webFarrock <webFarrock@mail.ru>
2017-03-20 09:21:31 +0300

    several changes

--- src/components/search-result-list.js
+++ src/components/search-result-list.js
@@ -1,31 +1,44 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {number_format} from '../tools/tools'
 
-class SearchResultList extends Component {
+export default class SearchResultList extends Component {
 
     constructor(props){
         console.log('SearchResultList constructor');
         super(props);
 
+        this.state = {
+            page: 1,
+            SEARCH_RAW: {},
+            SEARCH: {},
+            HOTELS_INFO: {},
+            isLoading: true,
+            isLoadingLT: true,
+            chkLTResNum: 0,
+            ltUsedOperators: [],
+            completedRequests: {},
+            boundsFirst: true,
+            isBtnOn: false,
+        };
+
+        this.NTK_API_IN = window.RuInturistStore.NTK_API_IN;
+        this.LL_API_IN = window.RuInturistStore.LL_API_IN;
+
+        this.ajaxUrl = '/tour-search/ajax.php';
+
+        this.arAjaxReq = []; // массив xhr обьектами
+
+        this.LTMaxChkNum = 8; // максимальное количество запросов к ЛЛ
+        this.LTChkTimeOut = 4 * 1000; // интервал проверки результатов ЛТ
+        this.itemsPerPage = 25; // кол-во элементов на стр
+
     }
 
     componentDidMount(){
-        $('.tour-search__map__hide').click(function () {
-            if (!$('.tour-search__map').hasClass('small')) {
-                $(this).addClass('expand').children('.label').text('Развернуть');
-                $(this).children('.icon-arrow-right').attr('class', 'icon-arrow-left');
-                $('.tour-search__map').addClass('small');
-                $('.tour-search__results').addClass('wide');
-            } else {
-                $(this).removeClass('expand').children('.label').text('Скрыть');
-                $(this).children('.icon-arrow-left').attr('class', 'icon-arrow-right');
-                $('.tour-search__map').removeClass('small');
-                if ($('.tour-search__results').hasClass('wide')) {
-                    $('.tour-search__results').removeClass('wide');
-                }
-            }
-        });
+
+        this.getNtkHotelList();
+
+
 
         function initMap() {
             //Set coordinates
@@ -65,7 +78,28 @@ class SearchResultList extends Component {
 
 
         jQuery(document).ready(function ($) {
+
+            $('.tour-search__map__hide').click(function () {
+                if (!$('.tour-search__map').hasClass('small')) {
+                    $(this).addClass('expand').children('.label').text('Развернуть');
+                    $(this).children('.icon-arrow-right').attr('class', 'icon-arrow-left');
+                    $('.tour-search__map').addClass('small');
+                    $('.tour-search__results').addClass('wide');
+                } else {
+                    $(this).removeClass('expand').children('.label').text('Скрыть');
+                    $(this).children('.icon-arrow-left').attr('class', 'icon-arrow-right');
+                    $('.tour-search__map').removeClass('small');
+                    if ($('.tour-search__results').hasClass('wide')) {
+                        $('.tour-search__results').removeClass('wide');
+                    }
+                }
+            });  
+            
+            console.log('');
+
+
             // scroll content
+            /*
             var pane = $('.scroll-content');
             pane.jScrollPane({
                 autoReinitialise: true
@@ -79,9 +113,158 @@ class SearchResultList extends Component {
                 return false;
             });
 
+*/
+
+
         });
 
     }
+
+    componentDidUpdate(){
+        $('.carousel-in').each(function () {
+            var $root = $(this);
+            var $thumbs = $root.next('.carousel-in-thumbs');
+
+            $root.slick({
+                accessibility: false,
+                dots: false,
+                arrows: true,
+                fade: true
+            });
+
+            $thumbs.on('click', '.list__item', function () {
+                $root.slick('slickGoTo', $(this).index());
+            });
+
+            $root.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+                $thumbs.find('li').eq(nextSlide).addClass('-active').siblings('li').removeClass('-active');
+            });
+        });
+    }
+
+
+    getNtkHotelList() {
+
+        console.log('getNtkHotelList: ');
+        console.log('this.NTK_API_IN: ', this.NTK_API_IN);
+        
+        if (this.NTK_API_IN.Destination){
+
+            var req = $.ajax({
+                url: this.ajaxUrl,
+                data: {
+                    NTK_API_IN: this.NTK_API_IN,
+                    ajax: 'Y',
+                },
+                dataType: 'json',
+                //dataType: 'text',
+                cache: false,
+            }).done(data => {
+
+
+
+                if (data && data.SEARCH instanceof Object) {
+
+                    let obSearch = Object.assign({}, this.state.SEARCH, data.SEARCH);
+                    this.setState({
+                        SEARCH: obSearch,
+                        HOTELS_INFO: Object.assign({}, this.state.HOTELS_INFO, data.HOTELS_INFO),
+                        USER_FAV: [].concat(data.USER_FAV, this.state.USER_FAV),
+                        isLoading: false
+                    });
+
+                }
+
+            });
+
+            this.arAjaxReq.push(req);
+        }
+    }
+
+    renderAttrs(attrs){
+
+        let  output = [];
+
+
+        if(attrs instanceof Object){
+            for (var key in attrs) {
+                output.push(
+                    <span className={key} title={attrs[key].join(', ')}></span>
+
+                )
+            }
+        }
+
+        if(output.length){
+            output = output.slice(0,5);
+            return(
+                <div className="hotel-option">{output}</div>
+            );
+        }
+
+    }
+
+    renderHotels(){
+
+        console.log('!!!!!!!!!!!this.state.SEARCH: ',this.state);
+
+        let search = Object.values(this.state.SEARCH);
+
+        search.filter(i => +i.Price > 0);
+        return search.map((hotel) => {
+            let hotelInfo = this.state.HOTELS_INFO[hotel.HOTEL_INFO_ID];
+
+            if(hotelInfo){
+                const priceForPrint = number_format(+hotel.Price, 0, '', ' ',);
+
+                return(
+                    <li className="list__item" key={hotel.HOTEL_INFO_ID}>
+                        <div className="row hotel-card">
+                            <div className="hotel-card__actions">
+                                <div className="hot-price show-description">
+                                    <span className="icon-hotprice"></span>
+                                    <div className="description">Горячая цена</div>
+                                </div>
+                                <div className="confirmation show-description">
+                                    <span className="icon-statim"></span>
+                                    <div className="description">Моментальное подтверждение</div>
+                                </div>
+                            </div>
+
+                            <div className="col__left hotel-card__image">
+                                <div className="carousel carousel-in">
+                                    {hotelInfo.IMAGES.map((i, j) => <div className="carousel__item" key={j}><img src={i} /></div>)}
+                                </div>
+                            </div>
+                            <div className="col__middle hotel-card__content">
+                                <div className="rating left -star-4"></div>
+                                <span className="icon-addfavorite right"></span>
+                                <span className="icon-newsletter right"></span>
+
+                                <h5 className="hotel-card__title" title={hotelInfo.NAME}>{hotelInfo.NAME}</h5>
+                                <div className="hotel-card__location">{hotelInfo.LOCATION}</div>
+
+                                    {this.renderAttrs(hotelInfo.arPreparedAttrs)}
+
+                                <div className="hotel-card__service">перелет, трансфер, завтрак</div>
+
+                                <div className="world-rating"></div>
+
+                                <a href="" className="button buy">
+                                <span className="buy-wrapper">
+                                    <span className="price"><i>от</i> {priceForPrint} <span className="rub">Р</span></span>
+                                </span>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                );
+            }
+
+        });
+
+    }
+
 
     render() {
         return (
@@ -125,53 +308,8 @@ class SearchResultList extends Component {
                                     <ul className="list -inline tour-search__results__list scroll-content">
 
 
-                                        {/* карточка */}
-                                        <li className="list__item">
-                                            <div className="row hotel-card">
-                                                <div className="hotel-card__actions">
-                                                    <div className="hot-price show-description">
-                                                        <span className="icon-hotprice"></span>
-                                                        <div className="description">Горячая цена</div>
-                                                    </div>
-                                                    <div className="confirmation show-description">
-                                                        <span className="icon-statim"></span>
-                                                        <div className="description">Моментальное подтверждение</div>
-                                                    </div>
-                                                </div>
+                                        {this.renderHotels()}
 
-                                                <div className="col__left hotel-card__image">
-                                                    <div className="carousel carousel-in">
-                                                        <div className="carousel__item"><img src="/local/tpl/dist/static/i/slideshow/hotel-seach-1.jpg" alt="" /></div>
-                                                        <div className="carousel__item"><img src="/local/tpl/dist/static/i/slideshow/hotel-seach-2.jpg" alt="" /></div>
-                                                        <div className="carousel__item"><img src="/local/tpl/dist/static/i/slideshow/hotel-seach-3.jpg" alt="" /></div>
-                                                    </div>
-                                                </div>
-                                                <div className="col__middle hotel-card__content">
-                                                    <div className="rating left -star-4"></div>
-                                                    <span className="icon-addfavorite right"></span>
-                                                    <span className="icon-newsletter right"></span>
-
-                                                    <h5 className="hotel-card__title">Max Royal Belek</h5>
-                                                    <div className="hotel-card__location">Турция, Кемер</div>
-                                                    <div className="hotel-option">
-                                                        <span className="icon-hotel-datailed-option-1 icon-pool"></span>
-                                                        <span className="icon-hotel-datailed-option-4 icon-wifi"></span>
-                                                        <span className="icon-hotel-datailed-option-5 icon-gym"></span>
-                                                        <span className="icon-hotel-datailed-option-9 icon-sea"></span>
-                                                        <span className="icon-hotel-datailed-option-10 icon-air-conditioning"></span>
-                                                    </div>
-                                                    <div className="hotel-card__service">перелет, трансфер, завтрак</div>
-
-                                                    <div className="world-rating"></div>
-
-                                                    <a href="" className="button buy">
-                                                    <span className="buy-wrapper">
-                                                        <span className="price"><i>от</i> 999 000 <span className="rub">Р</span></span>
-                                                    </span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </li>
                                     </ul>
                                     <div className="scroll-bottom">
                                         <span className="icon-arrow-bottom"></span>
@@ -190,17 +328,3 @@ class SearchResultList extends Component {
     }
 }
 
-
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-
-    }, dispatch);
-}
-
-function mapStateToProps(state) {
-    return {...state}
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchResultList);
--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
-
 import SearchResultList from './components/search-result-list';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-
+console.log('SearchResultList: ', SearchResultList);
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
         <SearchResultList />
-    </Provider>
     , document.querySelector('#react-search-result-list'));
